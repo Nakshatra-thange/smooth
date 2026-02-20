22980975882f9b2669588a42388777645b4641ac
Nakshatra-thange <nakshthange@gmail.com>
2026-02-20 12:42:06 +0530

    Fix prisma client import path

--- be/src/config/database.ts
+++ be/src/config/database.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "..//../generated/prisma/client"
+import { PrismaClient } from '../../prisma/generated/prisma/client'  // ✅ Correct
 import { PrismaPg } from "@prisma/adapter-pg";
 
 /**
